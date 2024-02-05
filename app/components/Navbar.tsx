f11c55507e38275b8d3240a6b184e0f5b61e206a
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-02-05 10:21:04 +0100

    [UPDATE] Cambio de cabecera y eliminado el shop cart

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -36,7 +36,7 @@ export default async function Navbar() {
   const links = await getLinks();
 
   return (
-    <header className="mb-8">
+    <header style={{height: 60, marginTop: 40}} className="mb-8">
       <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
         <Link href="/">
           <h1 className="text-2xl md:text-4xl font-bold">
@@ -64,7 +64,7 @@ export default async function Navbar() {
             </div>
           ))}
         </nav>
-        <div className="flex divide-x ">
+        {/* <div className="flex divide-x ">
           <Button
             variant={"outline"}
             onClick={() => handleCartClick()}
@@ -75,7 +75,7 @@ export default async function Navbar() {
               Cart
             </span>
           </Button>
-        </div>
+        </div> */}
       </div>
     </header>
   );
